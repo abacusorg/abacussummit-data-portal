a0eacdd0526a31fc0134b19cc1d255fe87b296a1
Lehman Garrison <lgarrison@cfa.harvard.edu>
2021-09-04 08:04:23 -0700

    Fix manifest for small power

--- build_manifest.py
+++ build_manifest.py
@@ -40,11 +40,12 @@ DEFAULT_OUTDIR = 'web/portal/static/data/'
 DEFAULT_REDSHIFTS = [0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.575, 0.65, 0.725, 0.8, 0.875, 0.95, 1.025, 1.1, 1.175, 1.25, 1.325, 1.4, 1.475, 1.55, 1.625, 1.7, 1.85, 2.0, 2.25, 2.5, 2.75, 3.0, 5.0, 8.0]
 
 def find_products(simdir, products, redshifts):
+    parent, child = simdir
     j = {}
     header = {}
     for prod in products:
         j[prod] = {}  # j['halos']
-        pdir = simdir.parent / products[prod]['path'].format(simdir.name)
+        pdir = parent / products[prod]['path'].format(child)
         for zdir in sorted(pdir.glob('z*/')):
             zval = zdir.name[1:]
             zval = float(zval)
@@ -170,11 +171,12 @@ def main(sim_pats=DEFAULT_SIM_PATS,
     #sims = [sim for i,sim in enumerate(sorted(sims)) if i == 0 or i > 2050]
     for sim in tqdm(sorted(sims)):
         sim = Path(sim)
+        slug = str(sim.relative_to(root))
         
-        row = find_products(sim, products, redshifts)
+        row = find_products((root, slug), products, redshifts)
         if row:
             row.update({'name': sim.name,
-                        'root': str(sim.relative_to(root)),
+                        'root': slug,
                         })
             rows += [row]
     
